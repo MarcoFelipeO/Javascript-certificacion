6c317ad8f9c6ec737ef970b6e73493168eb3716b
Marco Orellana Arriagada <marco.orellana@megamedia.cl>
2024-09-10 13:08:59 -0300

    changes

---
+++ mostrarDatos.js
@@ -0,0 +1,13 @@
+/* Escribe tu código aquí */
+function mostrarDatos(arreglo, n) {
+    // Declaramos 'i' con 'let' para evitar problemas de variables no declaradas
+    for ( i = 0; i<n; i++) {
+        console.log(arreglo[i]);
+    }
+}
+
+
+
+/* Fin */
+mostrarDatos([4,6,8,2,3],2)
+mostrarDatos([1,2,3,4,5,6],4)
